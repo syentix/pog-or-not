9b49bf9b9ae2d31968811d3f17038fe840a8a6d1
Syentix <syentixdev@protonmail.com>
2020-07-17 19:28:57 +0200

    Added counts of a hardcoded streamer :100:

---
+++ README.md
@@ -0,0 +1,11 @@
+# Pog or not?
+
+Website that counts the usage of emotes. Analyses the stream by pogness and 5headness...
+
+### TODO: 
+
+* [X] https://github.com/tmijs/tmi.js - To listen to chat.
+* [X] Get connection to chat.
+* [X] Listen to emotes and count them (ONCE per message)
+* [ ] Show ratio of emotes.
+* [ ] Give feedback to streamer.
---
+++ app.js
@@ -0,0 +1,79 @@
+//
+// ─── DEFINING SOME CONSTANTS ────────────────────────────────────────────────────
+//
+
+const POGCHAMP = ["pogchamp", "poggers", "pog"];
+const LUL = ["lul", "lol"];
+const FOURHEAD = ["4head", "5head"];
+
+//
+// ─── DEFINING NONSENSE EXCEPTION ────────────────────────────────────────────────
+//
+
+const BreakException = {};
+
+//
+// ─── GETTING SELECTOR FOR ELEMENTS IN HTML ──────────────────────────────────────
+//
+
+const versusElem = document.querySelector("#versus");
+
+//
+// ─── CONNECTING TO TWITCH CHAT ──────────────────────────────────────────────────
+//
+
+const client = new tmi.Client({
+  connection: {
+    secure: true,
+    reconnect: true,
+  },
+  channels: ["fl0m"],
+});
+
+client.connect().catch(console.error);
+
+//
+// ──────────────────────────────────────────────────────────────── CONNECTED ─────
+//
+
+//
+// ─── COUNTING ALL THE EMOTES ────────────────────────────────────────────────────
+//
+
+let counts = {
+  lul: 0,
+  fourHead: 0,
+  pog: 0,
+};
+
+// ────────────────────────────────────────────────────────────────────────────────
+
+client.on("message", (channel, tags, message, self) => {
+  if (self) return;
+  let messageParts = message.split(" ");
+  // Looping through every word in message
+  try {
+    messageParts.forEach((word) => {
+      if (POGCHAMP.includes(word.toLowerCase())) {
+        console.log(message);
+        counts.pog++;
+        throw BreakException;
+      } else if (FOURHEAD.includes(word.toLowerCase())) {
+        console.log(message);
+        counts.fourHead++;
+        throw BreakException;
+      } else if (LUL.includes(word.toLowerCase())) {
+        console.log(message);
+        counts.lul++;
+        throw BreakException;
+      }
+    });
+  } catch (e) {
+    if (e !== BreakException) console.log(e);
+  }
+  versusElem.textContent = `${counts.lul} LULs vs. ${counts.pog} POGs vs. ${counts.fourHead} 4Heads`;
+});
+
+//
+// ──────────────────────────────────────────────── COUNTED AND ADDED TO SITE ─────
+//
